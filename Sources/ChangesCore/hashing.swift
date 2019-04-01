d8a4014956d0b5ac09ec7bf8fd410d02eb4ff570
Paul Gowder <paul.gowder@gmail.com>
2019-03-31 19:55:40 -0500

    switching hash from string to data in order to be able to confidently handle binary types

--- Sources/ChangesCore/hashing.swift
+++ Sources/ChangesCore/hashing.swift
@@ -2,10 +2,11 @@ import Foundation
 import xxHash_Swift
 
 func getHash(_ filename: String) -> Int? {
-    let data = try? String(contentsOfFile: filename)
-    guard let toHash = data else {
+    let data = NSData(contentsOfFile: filename)
+    guard let unwrapped = data else {
         return nil
     }
+    let toHash = unwrapped as Data
     let hash = xxHash32.digest(toHash)
     return Int(hash)
 }
